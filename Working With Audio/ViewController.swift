32ea30d93a6aea968d2e18362e0778c372bcabce
Default Profile User <pbhanda1@fa146-image.pods.bu.int>
2017-02-07 16:27:56 -0500

    committing

--- Working With Audio/ViewController.swift
+++ Working With Audio/ViewController.swift
@@ -7,12 +7,42 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var slider: UISlider!
+    var player : AVAudioPlayer = AVAudioPlayer()
 
+    @IBAction func play(sender: AnyObject) {
+        
+       player.play()  
+        
+    }
+    @IBAction func pause(sender: AnyObject) {
+        
+        player.pause()
+    }
+    @IBAction func volume(sender: AnyObject) {
+        
+        player.volume = slider.value
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let audioPath = NSBundle.mainBundle().pathForResource("omgflute", ofType: "mp3")!
+        
+        do{
+            try player = AVAudioPlayer(contentsOfURL: NSURL(fileURLWithPath: audioPath))
+            
+        } catch {
+            //process error here
+        }
+
+        
+       
+        
     }
 
     override func didReceiveMemoryWarning() {
